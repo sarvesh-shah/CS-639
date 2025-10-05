7151d2f7d9e2d1d638e3b6994253325ba3e9e947
sarvesh-shah <sarveshshah2000@gmail.com>
2025-10-05 09:27:16 -0400

    About Me Changes

---
+++ About Me/app/src/main/java/com/example/aboutme/SecondFragment.java
@@ -0,0 +1,33 @@
+package com.example.aboutme;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+public class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        Button prevButton = view.findViewById(R.id.button_previous);
+
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.FirstFragment);
+            }
+        });
+
+        return view;
+    }
+}
